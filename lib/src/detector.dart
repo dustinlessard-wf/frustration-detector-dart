f4106b2bbc20f9bae31ccd13d5d93d5a8784b560
Dustin Pauze <dustin.pauze@workiva.com>
2022-12-10 05:59:51 -0500

    initial files

---
+++ lib/src/detector.dart
@@ -0,0 +1,11 @@
+class Detector {
+// todo: pattern to detect: rage clicking
+// todo: pattern to detect:random clicking (clicking on non-clickable elements)
+// todo: pattern to detect:rage scrolling
+// todo: pattern to detect:random scrolling
+// todo: pattern to detect:page refreshes/abandoned pages
+// todo: pattern to detect:back tapping
+// todo: pattern to detect:unresponsive UI/gestures
+// todo: pattern to detect:aborted operations
+
+}
