8ab632f6a476ac84adda9a496d763c5bdd09b0cd
Dustin Pauze <dustin.pauze@workiva.com>
2022-12-12 15:23:44 -0500

    wip

--- lib/src/detector.dart
+++ lib/src/detector.dart
@@ -5,6 +5,7 @@ import 'package:rxdart/rxdart.dart';
 class Detector {
   Detector() {
     // todo: pattern to detect: rage clicking, 3 clicks within 750ms
+    // reference : https://help.piwik.pro/support/collecting-data/rage-clicks-error-clicks-dead-clicks-and-mouse-shakes/
     final rawMouseClicks = Observable(window.document.onClick);
     rawMouseClicks
       ..buffer(onTime(const Duration(milliseconds: 750))).listen((clicks) {
@@ -16,8 +17,7 @@ class Detector {
       });
   }
 
-// todo: pattern to detect: dead clicking
-// todo: pattern to detect:random clicking (clicking on non-clickable elements)
+// todo: pattern to detect:dead/random clicking (clicking on non-clickable elements)
 // todo: pattern to detect:rage scrolling
 // todo: pattern to detect:random scrolling
 // todo: pattern to detect:page refreshes/abandoned pages
