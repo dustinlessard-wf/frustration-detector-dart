494ba301be4bb260e576063be579c9848368a5c5
Dustin Pauze <dustin.pauze@workiva.com>
2023-01-15 07:19:55 -0500

    Latest rxDart version, started erratic mouse movements

--- lib/src/detector.dart
+++ lib/src/detector.dart
@@ -2,19 +2,33 @@ import 'dart:html';
 
 import 'package:rxdart/rxdart.dart';
 
-class Detector {
-  Detector() {
-    // todo: pattern to detect: rage clicking, 3 clicks within 750ms
-    // reference : https://help.piwik.pro/support/collecting-data/rage-clicks-error-clicks-dead-clicks-and-mouse-shakes/
-    final rawMouseClicks = Observable(window.document.onClick);
-    rawMouseClicks
-      ..buffer(onTime(const Duration(milliseconds: 750))).listen((clicks) {
-        if (clicks.length >= 3) {
-          print('rage clicks');
-        } else {
-          print('nothing to do here');
-        }
-      });
+class FrustrationDetector {
+  FrustrationDetector() {
+    // pattern to detect: rage clicking, 3 clicks within 750ms
+    final rageClickPeriod =
+        Stream<dynamic>.periodic(const Duration(milliseconds: 750));
+    window.document.onClick.buffer(rageClickPeriod).listen((clicks) {
+      if (clicks.length >= 3) {
+        print('rage clicks');
+      } else {
+        print('nothing to do here');
+      }
+    });
+
+    // pattern to detect: erratic mouse movements, rapid changes in direction
+    // Detect changes
+    var velocity;
+    var direction;
+    var directionChangeCount = 0;
+    var distance = 0;
+    window.document.onMouseMove.listen((MouseEvent event) {
+      var newDirecion = event.offset.x.sign;
+      distance = event.offset.x.abs() + event.offset.y.abs();
+      if (newDirecion != direction) {
+        direction = newDirecion;
+        directionChangeCount++;
+      }
+    });
   }
 
 // todo: pattern to detect:dead/random clicking (clicking on non-clickable elements)
